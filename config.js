dc46597434b2845b14e6f01eb1c504210ff393d5
Walker <git@walkerselby.com>
2021-11-13 11:37:59 -0800

    Add segmentSpeedUrl

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ let config = {};
 config.appId = "f6rmtcl05k";
 config.hashToken = "ZjZybXRjbDA1a3w2YU55M3Yza2x1YW9LcFVCQWdiT1RzMXZhcFVqSXRYNjBNUVljeThm";
 config.authTokenUrl = "https://api.iq.inrix.com/auth/v1/appToken";
+config.segmentSpeedUrl = "https://api.iq.inrix.com/v1/segments/speed";
 module.exports = config;
-
