c255fab094b5fb5bacda5b030ed70c8895ffd237
Walker <git@walkerselby.com>
2021-11-13 11:19:04 -0800

    Initial Commit

---
+++ config.js
@@ -0,0 +1,8 @@
+//Code from https://github.com/INRIX/iq-api-samples/blob/master/api/config.js
+let config = {};
+
+config.appId = "f6rmtcl05k";
+config.hashToken = "ZjZybXRjbDA1a3w2YU55M3Yza2x1YW9LcFVCQWdiT1RzMXZhcFVqSXRYNjBNUVljeThm";
+config.authTokenUrl = "https://api.iq.inrix.com/auth/v1/appToken";
+module.exports = config;
+
